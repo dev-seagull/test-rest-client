b21be86ad1ada782e492762b07eb7ca76cbfbb08
Alireza Hosseini <alireza.hoseini20031382@gmail.com>
2022-07-08 14:20:51 +0430

    Undefined bugs were fixed

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect,useState } from 'react';
+import { StyleSheet, Text, View,StatusBar,TextInput,Button } from 'react-native';
 
 export default function App() {
   const [text, setText] = useState("")
-  const [name, setName] = useState("World!")
+  const [name, setName] = useState("Hello World!")
+
   const handlePressBTN = () => {
-    fetch('http://localhost:8080/greeting').then(response => response.json()).then(data => setName(data.name));
-    console.log(name)
+    fetch(`http://10.0.2.2:8080/greeting?name=${text}`)
+    .then(response => response.json())
+    .then(data => setName(data.content));
   }
+
+  useEffect(() => {console.log(name)},[name])
 
   return (
     <View style={styles.container}>
-      <Text style={styles.head}>Hello {name}</Text>
+      <Text style={styles.head}>Rest api Get method</Text>
       <TextInput
         style={styles.input}
         placeholder="Enter your name: "
@@ -23,8 +26,32 @@ export default function App() {
         title="Submit your Get request"
         onPress={handlePressBTN}
       />
-      <Text>Preview of input: {text}</Text>
+      <Text> Get output: {name}</Text>
       <StatusBar style="auto" />
     </View>
   );
 } 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  head: {
+    fontSize: 20,
+    margin: 20,
+    padding: 20
+  },
+  input: {
+    fontSize: 24,
+    border: 1,
+    borderColor:"black",
+    margin: 5
+  },
+  button: {
+    backgroundColor: "blue",
+    fontSize: 15,
+  }
+});
